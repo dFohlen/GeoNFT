410a52273711a83d5e857fefde5b8c85908e009f
Volker Dufner <dufnervolker@gmail.com>
2022-08-11 10:50:58 +0200

    Update Create Geocache API
    The geocache now requires and NFT which is placed at the current geolocation of the smartphone.

--- packages/app/src/api/createGeocache.ts
+++ packages/app/src/api/createGeocache.ts
@@ -1,29 +1,49 @@
-import { AnchorWallet } from '@solana/wallet-adapter-react';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { PublicKey, SYSVAR_RENT_PUBKEY } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, getAccount } from '@solana/spl-token';
 import * as anchor from '@project-serum/anchor';
-// import {IDL, NftGeocaching} from '@nft-geocaching/anchor/target/types/nft_geocaching'
-import idl from '@nft-geocaching/anchor/target/idl/nft_geocaching.json';
+import { NftGeocaching } from '@nft-geocaching/anchor/target/types/nft_geocaching';
 
-// export async function createGeocache(connection: Connection, wallet: AnchorWallet): Promise<anchor.IdlTypes<NftGeocaching>> {
-export async function createGeocache(connection: Connection, wallet: AnchorWallet): Promise<any> {
-    const provider = new anchor.AnchorProvider(connection, wallet, { preflightCommitment: 'processed' });
-    const programID = new PublicKey(idl.metadata.address);
-    const program = new anchor.Program(idl as anchor.Idl, programID, provider);
-    console.log(program);
-    const account = anchor.web3.Keypair.generate();
+export async function createGeocache(
+    program: anchor.Program<NftGeocaching>,
+    hiderTokenAccount: PublicKey,
+    mint: PublicKey,
+    location: GeolocationPosition
+) {
+    const newGeocache = anchor.web3.Keypair.generate();
 
-    const transaction = await program.methods.create().rpc;
-    await program.rpc.create({
-        accounts: {
-            geocache: account.publicKey,
-            player: wallet.publicKey,
+    // Vault PDA
+    const [vaultAddress, vaultBump] = await anchor.web3.PublicKey.findProgramAddress(
+        [newGeocache.publicKey.toBytes()],
+        program.programId
+    );
+
+    console.log('Vault PDA', vaultAddress.toString());
+    console.log('Vault bump', vaultBump.toString());
+
+    const hiderAccount = await getAccount(program.provider.connection, hiderTokenAccount);
+
+    console.log('Hider Account', hiderAccount);
+
+    if (hiderAccount.amount < 1) {
+        throw new Error('Hider account does not have enough tokens');
+    }
+
+    const tx = await program.methods
+        .createGeocache(vaultBump, location.coords.latitude + ',' + location.coords.longitude)
+        .accounts({
+            geocache: newGeocache.publicKey,
+            tokenAccount: vaultAddress,
+            hiderTokenAccount: hiderTokenAccount,
+            hider: (program.provider as anchor.AnchorProvider).wallet.publicKey,
+            mint: mint,
             systemProgram: anchor.web3.SystemProgram.programId,
-        },
-        signers: [account],
-    });
-    console.log('Transaction signature', transaction);
+            tokenProgram: TOKEN_PROGRAM_ID,
+            rent: SYSVAR_RENT_PUBKEY,
+        })
+        .signers([newGeocache])
+        .rpc();
+    console.log('Transaction signature', tx);
 
-    const geocache = await program.account.geocache.fetch(account.publicKey);
+    const geocache = await program.account.geocache.fetch(newGeocache.publicKey);
     console.log('Geocache', geocache);
-    return account;
 }
