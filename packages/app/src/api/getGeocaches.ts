2c4ad1e48d398d1c97581f7f39b2120c3cbd0008
Volker Dufner <dufnervolker@gmail.com>
2022-08-11 10:49:42 +0200

    Add Get Geocaches API
    This is used to retrieve all active geocaches.

---
+++ packages/app/src/api/getGeocaches.ts
@@ -0,0 +1,23 @@
+import { Program, ProgramAccount } from '@project-serum/anchor';
+import { NftGeocaching } from '@nft-geocaching/anchor/target/types/nft_geocaching';
+
+export async function getGeocaches(program: Program<NftGeocaching>): Promise<ProgramAccount[]> {
+    let geocaches = await program.account.geocache.all();
+    // const geocaches = await program.provider.connection.getParsedProgramAccounts(program.programId, {
+    //   filters: [
+    //     {
+    //       memcmp: {
+    //         offset: 72,
+    //         bytes: '1', // How to filter for active geocaches?
+    //     }
+    //    }
+    //   ]
+    // });
+    console.log('Geocaches', geocaches);
+
+    geocaches = geocaches.filter(({ account }) => {
+        return account.active ? true : false;
+    });
+
+    return geocaches;
+}
