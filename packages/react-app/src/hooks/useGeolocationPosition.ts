9efa656a177ff046299943a89764bf037648a693
Volker Dufner <dufnervolker@gmail.com>
2022-08-13 16:06:57 +0200

    Add Function to calc the Distance between tow Coordinates
    #1

--- packages/react-app/src/hooks/useGeolocationPosition.ts
+++ packages/react-app/src/hooks/useGeolocationPosition.ts
@@ -1,6 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useSnackbar } from 'notistack';
 
+function degreesToRadians(degrees: number) {
+    return (degrees * Math.PI) / 180;
+}
+
+export function distanceInKmBetweenEarthCoordinates(
+    latitude1: number,
+    longitude1: number,
+    latitude2: number,
+    longitude2: number
+) {
+    const earthRadiusKm = 6371;
+    const lat = degreesToRadians(latitude2 - latitude1);
+    const long = degreesToRadians(longitude2 - longitude1);
+    const a =
+        Math.sin(lat / 2) * Math.sin(lat / 2) +
+        Math.sin(long / 2) *
+            Math.sin(long / 2) *
+            Math.cos(degreesToRadians(latitude1)) *
+            Math.cos(degreesToRadians(latitude2));
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = earthRadiusKm * c;
+    return d;
+}
+
 export function useGeolocationPosition() {
     const [geolocationPosition, setGeolocationPosition] = useState<GeolocationPosition>();
     const { enqueueSnackbar } = useSnackbar();
