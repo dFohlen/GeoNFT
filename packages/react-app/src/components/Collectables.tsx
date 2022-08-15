51ac18c0d7e210c5c765bb1b30f9af0f8544a81d
tim565 <schmitz.tim@protonmail.com>
2022-08-15 17:30:49 +0200

    NFTsList in seperate file

--- packages/react-app/src/components/Collectables.tsx
+++ packages/react-app/src/components/Collectables.tsx
@@ -5,41 +5,36 @@ import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { ProgramAccount } from '@project-serum/anchor';
 import { getParsedTokenAccountsByOwner, getMetadataByTokenAccounts } from '../api/getNFTs';
 import { createGeocache } from '../api/createGeocache';
 import { useGeolocationPosition } from '../hooks/useGeolocationPosition';
 import { ParsedAccountData } from '@solana/web3.js';
 import { useAnchor } from '../hooks/useAnchor';
 import { useSnackbar } from 'notistack';
+import { NftGeocaching } from '@nft-geocaching/anchor/target/types/nft_geocaching';
+import NFTsList from './NFTsList'
 
 export default function Collectables() {
     const { enqueueSnackbar } = useSnackbar();
-    const [nfts, setNfts] = useState<ParsedAccountData[]>();
+    //const [nfts, setNfts] = useState<ParsedAccountData[]>();
+    const [nfts, setNfts] = useState<ProgramAccount<NftGeocaching>[]>();
     const program = useAnchor();
     const location = useGeolocationPosition();
-    let nftsList:any;
 
     useEffect(() => {
         if (!nfts && location) {
             const fetchData = async () => {
                 const accounts = await getParsedTokenAccountsByOwner(program);
-                console.log('Token accounts: ');
+                console.log("rawNFTS:");
                 console.log(accounts);
-
-                nftsList = [accounts.length];
-
-                for(let i=0; i<accounts.length; i++){
-                    console.log(i);
-                    console.log(accounts[i].pubkey.toString());
-                    nftsList[i] = accounts[i];
-                }
-/*
-                const nftList = accounts.map((nft: ParsedAccountData) => {
-                    const nftPubKey = nft.account.pubkey.toString;
-                    return{
-                        nft
-                    };
-                })*/
+                const preparedNFTs = accounts
+                    .map((nft: any) => {
+                        return {
+                            nft
+                        };
+                    });
+                setNfts(preparedNFTs);
             };
 
             // call the function
@@ -49,6 +44,7 @@ export default function Collectables() {
         }
     }, [location, program, nfts]);
 
+/*
     const onClick = useCallback(async () => {
         if (!program.provider.publicKey) {
             throw new WalletNotConnectedError();
@@ -76,6 +72,7 @@ export default function Collectables() {
         // const geocache = await setGeocacheLocation(connection, wallet, account, location);
         console.log(geocache);
     }, [program, location, enqueueSnackbar]);
+    */
 
     return (
         <>
@@ -86,18 +83,9 @@ export default function Collectables() {
             ) : (
                 <div>
                     <p>Choose a NFT to create a Geocache</p>
-                    {nftsList?.map((nft:any) => (
-                        <ListItem
-                            key={nft.pubKey.toString()}
-                        >
-                            <ListItemText
-                                primary="test"
-                            />
-                        </ListItem>
-                    ))}
+                    <NFTsList nfts={nfts}></NFTsList>
                 </div>
             )}
         </>
     );
 }
-
