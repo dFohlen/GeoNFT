b3b4bff8111cfddc33887eeaa7782ff457aeea49
Volker Dufner <dufnervolker@gmail.com>
2022-08-16 13:00:38 +0200

    Improve Loading/Selecting NFTs

--- packages/react-app/src/components/Collectables.tsx
+++ packages/react-app/src/components/Collectables.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
 import Grid from '@mui/material/Grid';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -43,61 +43,41 @@ export default function Collectables() {
             throw new Error('No location');
         }
 
-        const accounts = await getParsedTokenAccountsByOwner(program);
-        console.log(accounts);
-
-        if (accounts.length === 0) {
-            enqueueSnackbar('No accounts', { variant: 'error' });
-        }
-
-        for (let i=0; i<accounts.length; i++) {
-            if (chosenPubkey == accounts[i].pubkey.toString()) {
-                // const metadata = await getMetadataByTokenAccounts(accounts);
-                // console.log(metadata);
-                // const geocache = await setGeocacheLocation(connection, wallet, account, location);
-
-                const geocache = await createGeocache(
-                    program,
-                    accounts[i].pubkey,
-                    (accounts[0].account.data as ParsedAccountData).parsed.info.mint,
-                    location
-                );
-                console.log('Create: ', chosenPubkey);
-            }
-        }
-    }
+        nfts?.filter((nft: any) => nft.pubkey.toString() === chosenPubkey).forEach(async (nft: any) => {
+            const geocache = await createGeocache(
+                program,
+                nft.pubkey,
+                (nft.account.data as ParsedAccountData).parsed.info.mint,
+                location
+            );
+            console.log('Create: ', chosenPubkey);
+        });
+    };
 
     return (
         <>
-            {!location ? (
-                <div>
-                    <p>Loading location...</p>
-                </div>
-            ) : !nfts || nfts.length === 0 ? (
-                <div>
-                    <p>No NFTs found</p>
-                </div>
+           {!nfts ? (
+                <Typography>Loading NFTs...</Typography>
+            ) : nfts.length === 0 ? (
+                <Typography>No NFTs found</Typography>
             ) : (
                 <div>
-                    <Box m={2}>
-                        <p>Choose a NFT to create a Geocache</p>
-                        <List dense={true} sx={{ width: '100%', maxWidth: 360 }}>
-                            {nfts.map((item: any) => (
-                                <ListItemButton
-                                    key={item.pubkey}
-                                    divider={true}
-                                    onClick={() => newGeocache(item.pubkey.toString())}
-                                >
-                                    <img
-                                        src={"path"}
-                                        alt={"nft"}
-                                    />
-                                    <ListItemText primary={item.pubkey.toString()}/>
-                                </ListItemButton>
-                            ))}
-                        </List>
-                    </Box>
-                </div>
+                <Box m={2}>
+                    <p>Choose a NFT to create a Geocache</p>
+                    <List dense={true} sx={{ width: '100%', maxWidth: 360 }}>
+                        {nfts.map((item: any) => (
+                            <ListItemButton
+                                key={item.pubkey}
+                                divider={true}
+                                onClick={() => newGeocache(item.pubkey.toString())}
+                            >
+                                <img src={'path'} alt={'nft'} />
+                                <ListItemText primary={item.pubkey.toString()} />
+                            </ListItemButton>
+                        ))}
+                    </List>
+                </Box>
+            </div>
             )}
         </>
     );
